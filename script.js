0042a15b0aa42e9ac9a308121342524a503fee52
Qazi-Ghulam-Mustafa <Qazi-Ghulam-Mustafa>
2025-09-21 21:54:03 +0500

    Temporary Commit

---
+++ script.js
@@ -0,0 +1,183 @@
+
+// var num= prompt("How much time i print table of 9");
+// for(index=1; index<21; index++ ){
+//     console.log(index*9)
+// }
+
+
+// this is Array and the index of this array is 6 and length of this array is 7 
+
+// var continents = ["Asia", "Africa", "Europe", "Antartica", "North america", "South America", "Australia"];
+// for(index=0 ; index < continents.length; index++ ){
+//     console.log(continents[index])
+// }
+
+
+
+// Print deep and end from this nestedArray into console.
+// const nestedArray = [
+//     "Level1",
+//     42,
+//     true,
+//     [
+//         "Level2",
+//         false,
+//         99,
+//         [
+//             "Level3",
+//             123,
+//             true,
+//             [
+//                 "level4",
+//                 "deep",
+//                 false,
+//                 777,
+//                 [
+//                     "Level5",
+//                     1001,
+//                     true,
+//                     "end",
+//                 ]
+//             ]
+//         ]
+
+//     ]
+
+// ]
+// console.log(nestedArray[3][3][3][4][0], nestedArray[3][3][3][4][3]);
+
+// var nestedArray2=[
+//        "Mustafa","Moeez", "Ali",420,
+//        [
+//         tr
+//        ]
+// ]
+
+// var OneDimensionalArray=[1,2,"js"]
+// for()
+// var twoArrayDimesionalArray=[
+//     [],[],[]
+// ]
+// var ThreeDimesionalArray=[
+//      [ [],[],[] ] 
+// ]
+// var FourDimensionalArray=[
+//      [ [ [],[],[] ] ] 
+// ]
+
+// var arr=[
+//      ["Mustafa"],["Abdullah"],["Sarim"]
+// ]
+
+// for(var i=0; i < arr.length; i++ ){
+//      for(var j=0; j < arr [i].length; j++ );
+//           console.log(arr[i],[j],[j]);
+// }
+// var name="mustafa"
+// console.log(name)
+
+
+
+// this is Two Dimensional array 
+// let arr = [
+//      [
+//           ["Qazi"], ["Ghulam"], ["Mustafa"]
+//      ]
+// ]
+
+
+// for (let i=0; i<arr.length; arr++){
+//      for(let j=0; j < arr[i].length; j++){
+//      for(let k=0; k< arr[i],[j],length; k++){
+//      console.log(arr[0][0], arr[0][1], arr[0][2])
+// }
+// }
+// }
+
+
+// // This is three dimensional arrya
+// let arr2 = [
+//      [
+//           [
+//                ["Qazi"], ["Ghulam"], ["Mustafa"]
+//           ]
+//      ]
+// ]
+// console.log(arr2[0][0][0], arr2[0][0][1], arr2[0][0][2])
+
+
+// Create a array of random numbers
+//  find out the largest number from the given array   
+
+
+// let arr = [25, 78, 34, 89, 26]
+// let largestNum = []
+
+// for (let i = 0; i < arr.length; i++) {
+//      if (arr[i] > largestNum) {
+//           largestNum = arr[i]
+//      }
+// }
+// console.log(largestNum)
+
+
+// create an array of a random numbers
+// find out the largest number from the given array
+
+// let arr1=[10,20,40,88,5]
+// let smalletNum=0;
+// for( let i = 0; i < arr1.length; i++ ){
+//      if(arr1[i]< smalletNum){
+//           smalletNum = arr1[i]
+//      }
+// }
+// console.log(smalletNum)
+
+
+// create an array of random num
+// reverse the array num
+
+// var num = [1, 2, 3, 4, 5]
+// var reverse = []
+// for (var i = num.length - 1; i >= 0; i--) {
+//      console.log(num[i])
+// }
+
+// create an array of random numbers and find out the repeated number from an array
+
+
+// var userInput = +prompt("Enter a number");
+// var number = [1, 2, 1, 3, 1, 4, 3, 2, 5, 4, 6]
+// var count = []
+// for (var i = 0; i=number.length; i++ ){
+//      if( userInput == number[i] ){
+//              count = count + 1
+//     }
+// }
+// console.log(count)
+
+
+// revese your name
+
+// var name = ["Mustafa"]
+// for(var i = name.length ; i>=0; i--){
+//   console.log(name[i])
+// }
+
+// Create an array of random numbers and calculate average of number
+
+// var number=[5,10,15,20]
+// var sum= 0
+// for( var i=0; i< number.length; i++ ){
+//      sum = sum+number[i]
+// }
+// console.log(sum/number.length)
+
+// create an array of random numbers and make it double of each
+
+var num=[5,10,15,20]
+var double=0
+for( var i=0; i<num.length; i++){
+
+console.log(num[i]*2)
+}``
